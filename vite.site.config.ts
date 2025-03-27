c98f63b87dca73846a58cd3fef07038b2e910f98
Alex Veenendaal <alex@opendoorgonorth.com>
2025-03-27 22:07:33 +0000

    chore: github deploy action

---
+++ vite.site.config.ts
@@ -0,0 +1,26 @@
+import path from 'node:path';
+import preact from '@preact/preset-vite';
+import { defineConfig } from 'vite';
+import pkg from './package.json' with { type: 'json' };
+
+export default defineConfig({
+  build: {
+    emptyOutDir: true,
+    outDir: 'dist-site'
+  },
+  define: {
+    __API_URL__: 'window.__backend_api_url',
+    __APP_VERSION__: JSON.stringify(pkg.version)
+  },
+  plugins: [preact()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@helpers': path.resolve(__dirname, './src/helpers'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@lib': path.resolve(__dirname, './src/lib'),
+      '@model': path.resolve(__dirname, './src/model')
+    }
+  },
+  root: '.'
+});
