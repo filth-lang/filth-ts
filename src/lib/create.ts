21309e51bc716df3319c2e6c852834eb29b993c4
Alex Veenendaal <alex@opendoorgonorth.com>
2025-04-01 07:25:48 +0100

    chore: custom matcher

--- src/lib/create.ts
+++ src/lib/create.ts
@@ -57,7 +57,7 @@ const defineLogging = (env: EvalEnvironment) => {
       console.debug('[FILTH]', ...args);
       return null;
     },
-    { skipEvaluateArgs: true }
+    { skipEvaluateArgs: false }
   );
 };
 
